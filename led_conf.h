025d6f07cc276170e27480cbc4cd3a0d46f4e5dd
satyamss7 <80305407+satyamss7@users.noreply.github.com>
2023-02-08 21:20:10 +0530

    Update led_conf.h

--- led_conf.h
+++ led_conf.h
@@ -2,18 +2,14 @@ int set_gpio(char pin, char led_stat);
 int gpio_init();
 
 
-static int
-GPIOExport(int pin);
+static int GPIOExport(int pin);
 
 static int GPIOUnexport(int pin);
 
-static int
-GPIODirection(int pin, int dir);
+static int GPIODirection(int pin, int dir);
 
-static int
-GPIORead(int pin);
+static int GPIORead(int pin);
 
-static int
-GPIOWrite(int pin, int value);
+static int GPIOWrite(int pin, int value);
 
 
